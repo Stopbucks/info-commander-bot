25f583e6ee079988268a5b7c1c008b0c6c2b90fe
stopbucks <manchester.hu@example.com>
2026-03-04 23:33:02 +0800

    add 3 FLY_LAX to member

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from urllib.parse import urlparse
 app = Flask(__name__)
 
 # === 🎖️ 情報控制面板 ===
-INTEL_AUDIO_OFFICERS = ["ZEABUR", "RENDER"]
+INTEL_AUDIO_OFFICERS = ["ZEABUR", "FLY_LAX", "RENDER"]
 INTEL_TXT_OFFICERS = ["ZEABUR","KOYEB", "RENDER", "HUGGINGFACE"]
 
 CONFIG = {
